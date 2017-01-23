3ed6616678fd9e8b4f030909f4969e67729c38de
Nadim Rahman <n.m.rahman@se16.qmul.ac.uk>
2017-01-23 19:34:11 +0000

    Add files via upload
    Added changes to make BLAST output look better

--- Local_BLAST_v1.py
+++ Local_BLAST_v1.py
@@ -7,7 +7,15 @@
 
 # A quick way of BLASTing and writing into an XML output file of the results
  
-blastall -i p53.fasta.txt -p blastp -o Test_Result.txt 
+
+blastall -i p53.fasta.txt -d human.1.protein.faa -p blastp -o "Result.txt" # This produces the output (in a text file) in exactly the same way as online BLAST.
+
+blastall -i p53.fasta.txt -d human.1.protein.faa -p blastp -o "Results" -m 7
+
+blastall -i Sample.fasta -d human.1.protein.faa -p blastp -b 10 -m 8 > blast.out # Produces a blast.out file with the scores, e-values etc.
+# This can be used to generate a table to show on the web page.
+
+
 
 
 # The best way of BLASTing
