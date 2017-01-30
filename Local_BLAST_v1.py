20ba384c71dce505fc65ac24b9605c7d938fc59f
Nadim Rahman <n.m.rahman@se16.qmul.ac.uk>
2017-01-30 11:30:58 +0000

    Add files via upload

--- Local_BLAST_v1.py
+++ Local_BLAST_v1.py
@@ -6,24 +6,30 @@
 ##########################
 
 # A quick way of BLASTing and writing into an XML output file of the results
- 
+# First download the download the database - nt for nucleotides. Then using formatdb, format the database to use for the BLAST.
+# The database can be downloaded from NCBI's FTP site of databases or using the wget command and pasting the URL.
 
-blastall -i p53.fasta.txt -d human.1.protein.faa -p blastp -o "Result.txt" # This produces the output (in a text file) in exactly the same way as online BLAST.
+#formatdb -i nt -p F -V # -p F is for nucleotides and -V means verbose.
 
-blastall -i p53.fasta.txt -d human.1.protein.faa -p blastp -o "Results" -m 7
-
-blastall -i Sample.fasta -d human.1.protein.faa -p blastp -b 10 -m 8 > blast.out # Produces a blast.out file with the scores, e-values etc.
-# This can be used to generate a table to show on the web page.
-
-
+# The function to BLAST a fasta file.
+def blastFastaFile (fastaFile):
+	blastall -i fastafile -d nt -p blastn -b 10 -m 8 > blast.out
+	blastall -i Example.fasta -d nt -p blastn -b 10 -m 7 -o "Local_Blast_Output.xml" # This produces an XML file instead.
 
 
-# The best way of BLASTing
 
-from Bio.Blast.Applications import NcbiblastxCommandline
+
+############################
+# The best way of BLASTing #
+############################
+
+
+#import sys
+#sys.path.append('/mnt/c/Users/Nadim/Downloads/biopython-1.68')
+#from Bio.Blast.Applications import NcbiblastxCommandline
 # help(NcbiblastxCommandline)
 
-blastx_cline = NcbiblastxCommandline(query="p53.fasta", db="nr", evalue=0.001, outfmt=5, out="output.xml")
-blastx_cline
-print(blastx_cline)
-stdout, stderr = blastx_cline()
+#blastx_cline = NcbiblastxCommandline(query="Example.fasta", db="nt", evalue=0.001, outfmt=7, out="output.xml")
+#blastx_cline
+#print(blastx_cline)
+#stdout, stderr = blastx_cline()
