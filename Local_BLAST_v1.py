80f15c0d668d7c915f443c34df9f0c3b07832b0b
Nadim Rahman <n.m.rahman@se16.qmul.ac.uk>
2017-01-31 14:38:04 +0000

    Add files via upload
    Updated files with comments providing slightly more detailed instructions.

--- Local_BLAST_v1.py
+++ Local_BLAST_v1.py
@@ -5,13 +5,17 @@
 #      Local BLAST       #
 ##########################
 
+# Make sure you have BLAST+ downloaded from https://blast.ncbi.nlm.nih.gov/Blast.cgi?PAGE_TYPE=BlastDocs&DOC_TYPE=Download
 # A quick way of BLASTing and writing into an XML output file of the results
 # First download the download the database - nt for nucleotides. Then using formatdb, format the database to use for the BLAST.
-# The database can be downloaded from NCBI's FTP site of databases or using the wget command and pasting the URL.
+# The database can be downloaded from NCBI's FTP site of databases (ftp://ftp.ncbi.nlm.nih.gov/blast/db/FASTA/) and choose the nt.gz file to download.
+# Alternatively you can use the wget command with the URL.
 
-#formatdb -i nt -p F -V # -p F is for nucleotides and -V means verbose.
+#  Once downloaded, the database must be formatted using the following command:
+formatdb -i nt -p F -V # -p F is for nucleotides and -V means verbose.
 
 # The function to BLAST a fasta file.
+# Use one blastall function, NOT both, whichever suits your fancy :)
 def blastFastaFile (fastaFile):
 	blastall -i fastafile -d nt -p blastn -b 10 -m 8 > blast.out
 	blastall -i Example.fasta -d nt -p blastn -b 1 -m 7 -o "Local_Blast_Output.xml" # This produces an XML file instead.
@@ -22,8 +26,10 @@ def blastFastaFile (fastaFile):
 # -b = The number of results you want to show.
 # -m = The format of the output file (7 = XML, 8 = Tabular)
 # -o = The name of the output file.
+# blast.out is the output file.
 
 
+# Ignore the following:
 
 ############################
 # The best way of BLASTing #
--- Online_BLAST.py
+++ Online_BLAST.py
@@ -2,9 +2,9 @@
 
 import sys
 sys.path.append('/mnt/c/Users/Nadim/Downloads/biopython-1.68') # Required to bring Biopython into the python environment.
+# Just paste the path to the biopython-1.68 (or equivalent file) into the round brackets above.
 
 from Bio.Blast import NCBIWWW, NCBIXML
-
 
 
 
@@ -13,16 +13,13 @@ from Bio.Blast import NCBIWWW, NCBIXML
 ##########################
 
 
-fasta_string = open("noHeaderSample.fasta").read()
-result_handle = NCBIWWW.qblast("blastn", "nt", fasta_string) 
+fasta_string = open("Example.fasta").read()
+result_handle = NCBIWWW.qblast("blastn", "nt", fasta_string) # Carries out the BLAST search. 
 
-save_file = open("myBlast_v2.xml", "w") 
+save_file = open("myBlast_v2.xml", "w") # Opens a file to write the results to.
 save_file.write(result_handle.read())
 save_file.close()
-
-result_handle.close()
-result_handle = open("myBlast_v2.xml") # To open the output file.
-
+result_handle.close() # Closing both input and output files.
 
 
 
@@ -33,8 +30,9 @@ result_handle = open("myBlast_v2.xml") # To open the output file.
 
 blast_records = NCBIXML.read(result_handle) # .read is required for a single sequence. For multiple sequences, use .parse instead.
 
+# To print the alignments:
 eValThres = 0.04
-for alignment in blast_record.alignments:
+for alignment in blast_records.alignments:
 	for hsp in alignment.hsps:
 		if hsp.expect < eValThres:
 			print('****Alignment****')
@@ -46,6 +44,7 @@ for alignment in blast_record.alignments:
 			else:
 				dots = ''
 			print(hsp.query[0:75] + dots)
-			print(hsp.match[0.75] + dots)
+			print(hsp.match[0:75] + dots)
 			print(hsp.sbjct[0:75] + dots)
 
+result_handle.close()
