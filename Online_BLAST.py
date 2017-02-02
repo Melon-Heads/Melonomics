c45e8a9ad96b445aa4295e048f2dc0b89ed92285
Nadim Rahman <n.m.rahman@se16.qmul.ac.uk>
2017-02-02 12:21:36 +0000

    Add files via upload
    Final Online BLAST script to be used. This is an alternative if databases can't be downloaded.

--- Online_BLAST.py
+++ Online_BLAST.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python
 
+# This script requires:
+# --> BioPython
+
 import sys
 sys.path.append('/mnt/c/Users/Nadim/Downloads/biopython-1.68') # Required to bring Biopython into the python environment.
-# Just paste the path to the biopython-1.68 (or equivalent file) into the round brackets above.
+# Just paste the path to the biopython-1.68 (or equivalent) file.
 
-from Bio.Blast import NCBIWWW, NCBIXML
+from Bio.Blast import NCBIWWW, NCBIXML # For the BLAST and reading of XML file.
 
 
 
@@ -13,14 +16,14 @@ from Bio.Blast import NCBIWWW, NCBIXML
 ##########################
 
 
-fasta_string = open("Example.fasta").read()
-result_handle = NCBIWWW.qblast("blastn", "nt", fasta_string) # Carries out the BLAST search. 
+def blastFastaFile(fastaFile):
+	fasta_string = open(fastaFile).read()
+	result_handle = NCBIWWW.qblast("blastn", "nt", fasta_string) # Carries out the BLAST search with specified parameters. 
 
-save_file = open("myBlast_v2.xml", "w") # Opens a file to write the results to.
-save_file.write(result_handle.read())
-save_file.close()
-result_handle.close() # Closing both input and output files.
-
+	save_file = open(fastaFile"_Output.xml", "w") # Opens a file to write the results to.
+	save_file.write(result_handle.read())
+	save_file.close()
+	result_handle.close() # Closing both input and output files.
 
 
 ############################
@@ -28,23 +31,23 @@ result_handle.close() # Closing both input and output files.
 ############################
 
 
-blast_records = NCBIXML.read(result_handle) # .read is required for a single sequence. For multiple sequences, use .parse instead.
+	blast_records = NCBIXML.read(result_handle) # .read is required for a single sequence. For multiple sequences, use .parse instead.
 
-# To print the alignments:
-eValThres = 0.04
-for alignment in blast_records.alignments:
-	for hsp in alignment.hsps:
-		if hsp.expect < eValThres:
-			print('****Alignment****')
-			print('Sequence:', alignment.title)
-			print('Length:', alignment.length)
-			print('E-Value:', hsp.expect)
-			if len(hsp.query) > 75:
-				dots = '...'
-			else:
-				dots = ''
-			print(hsp.query[0:75] + dots)
-			print(hsp.match[0:75] + dots)
-			print(hsp.sbjct[0:75] + dots)
+	# To print the alignments:
+	eValThres = 0.04
+	for alignment in blast_records.alignments:
+		for hsp in alignment.hsps:
+			if hsp.expect < eValThres:
+				print('****Alignment****')
+				print('Sequence:', alignment.title)
+				print('Length:', alignment.length)
+				print('E-Value:', hsp.expect)
+				if len(hsp.query) > 75:
+					dots = '...'
+				else:
+					dots = ''
+				print(hsp.query[0:75] + dots)
+				print(hsp.match[0:75] + dots)
+				print(hsp.sbjct[0:75] + dots)
 
-result_handle.close()
+	result_handle.close()
