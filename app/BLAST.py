f45caa192f04a5d1f814fbf0e020c9601c2e040b
Nadim Rahman <n.m.rahman@se16.qmul.ac.uk>
2017-02-10 21:59:34 +0000

    Add files via upload
    This consists of changes implementing a class vector style to uploaded files depending on the sample type.

--- app/BLAST.py
+++ app/BLAST.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env/ python
 
-import sys, re, csv, os
+import sys, re, csv, os, glob
 
 # The following is for those who need to append biopython packages to the Python PATH:
 sys.path.append('/mnt/c/Users/Nadim/Downloads/biopython-1.68')
@@ -12,7 +12,6 @@ from Bio.Blast.Applications import NcbiblastnCommandline
 ###########################
 #       Local BLAST       #
 ###########################
-
 
 
 # This script REQUIRES:
@@ -40,7 +39,7 @@ def fastaBLAST(fastaFile):
 
 # Parameters for blastall and command line blast:
 # --> -i or query [Input fasta file]
-# --> -d or db [Database to compare against]
+# --> -d or db [Path to Database to compare against]
 # --> -p [Type of blast carried out (blastn for nucleotides)]
 # --> -b or max_target_seqs [Number of hits to show]
 # --> -m [Format of the output file (Non-command line: 7 = XML, 8 = Tabular; Command line = 5, 6 respectively)]
@@ -55,33 +54,38 @@ def fastaBLAST(fastaFile):
 
 
 samDict = {} # This needs to be global to add multiple blast results.
+vecList = [] # A list to add the sample code for class vectors.
 
 # Function to create dictionaries of the gene IDs and FPKM value per sample.
-def createDict(blastOut):
-        global samDict
+def createDict(blastOut, sampleCode):
+        global samDict, vecList
         inputBlast = open(blastOut, "r") # Read in the file passed through the function.
 
         # Lists to append gene IDs and FPKM values to.
         genIDs = []
         FPKMvals = []
+	# Append the sample code to the globally defined list.
+	vecList.append(sampleCode)
 
         # Dictionary of gene and FPKM values.
         genFPKM = {}
 
         for word in inputBlast:
                 getGen = re.findall('[a-zA-Z]\w\w+\.\w+', str(word)) # Regular expression used to obtain the gene IDs.
-                getFPKM = re.findall('\d+\.\d+', str(word)) # Regular expression to obtain the FPKM values.
-
                 genIDs.append(getGen)
-                FPKMvals.append(getFPKM)
 
-                genAcc = [l[0] for l in genIDs] # Remove any lists within the list.
-                FPKM = [l[0] for l in FPKMvals] # Removes any lists within the list.
+		m = re.match(r"asmbl_\d+;(\d+\.\d+)", str(word)) # Regular expression to obtain the FPKM values.
+		if m:
+			getFPKM = m.group(1)
+			FPKMvals.append(getFPKM)
+				
+                genAcc = [l[0] for l in genIDs] # Remove any lists within the gene ID list.
+        
+	# Join the gene ID and corresponding FPKM values
+	genFPKM.update(zip(genAcc, FPKMvals))
 
-        genFPKM.update(zip(genAcc, FPKM))
         # Join the gene ID and FPKM dictionary to a separate dictionary which specifies each sample.
         samDict.update({blastOut: genFPKM})
-
 
 
 # ---------------------------------------------------------------------
@@ -90,28 +94,53 @@ def createDict(blastOut):
 
 
 def dictToMatrix(dict):
+	
+	# A normal matrix from the dictionary formed.
         mat = pd.DataFrame(dict)
-        matCSV = mat.fillna(0) # This converts empty cells to display 0.
+	matCSV = mat.fillna(0) # This converts empty cells to display 0.
+	
+	# Another matrix with the class vector mentioned in the second row.		
+	vec = pd.DataFrame(dict)
+        vec.loc[1] = vecList	
+	vec = vec.sort()
+	vecCSV = vec.fillna(0)
+
+	# Create the CSV files.
         matCSV.to_csv('Gene_FPKM_Sample.csv')
+	vecCSV.to_csv('Class_Vector.csv')
+
+
+# ===================================================================== #
 
 
 
-# Look inside the data folder to BLAST all contents and create a dictionary.
-for file in os.listdir("/mnt/c/Users/Nadim/Documents/QMUL_Level7/Group_Software_Project/Melonomics/flask/venv/data"):
-	if file.endswith(".fasta"):
-		fastaBLAST(file)
-		createDict(file)
+# To call the functions, the correct files must be chosen, found in the specific folders:
+# The fasta files are sent to the BLAST function.
+# The BLAST output files are utilised to create a dictionary which will be used to make a matrix.
+
+os.chdir("/mnt/c/Users/Nadim/Documents/QMUL_Level7/Group_Software_Project/Melonomics/flask/venv/data/CTRL")
+for file in glob.glob("*.fasta"):
+	fastaBLAST(file)
+for file in glob.glob("*Output.txt"):
+	CTRLcode = 1
+	createDict(file, CTRLcode)
 
 
-# Functions to BLAST uploaded files:
-#fastaBLAST('0hourR1.fasta')
-#fastaBLAST('24hourR1.fasta')
-#fastaBLAST('8hourR1.fasta')
+os.chdir("/mnt/c/Users/Nadim/Documents/QMUL_Level7/Group_Software_Project/Melonomics/flask/venv/data/DS1")
+for file in glob.glob("*.fasta"):
+	fastaBLAST(file)
+for file in glob.glob("*Output.txt"):
+	DS1code = 2
+	createDict(file, DS1code)
 
-# Functions to process the BLAST output:
-#createDict('0hourR1.fasta_Output.txt')
-#createDict('8hourR1.fasta_Output.txt')
-#createDict('24hourR1.fasta_Output.txt')
+
+os.chdir("/mnt/c/Users/Nadim/Documents/QMUL_Level7/Group_Software_Project/Melonomics/flask/venv/data/DS2")
+for file in glob.glob("*.fasta"):
+        fastaBLAST(file)
+for file in glob.glob("*Output.txt"):
+	DS2code = 3
+	createDict(file, DS2code)
+
 
 dictToMatrix(samDict)
 
